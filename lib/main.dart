611f158b37e5a3f95e1db3f612465a2397a2b4ce
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2023-01-02 01:40:51 +0900

    5강

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,22 +11,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("앱임")),
-        body: Text("안녕"),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 50,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Icon(Icons.phone),
-                  Icon(Icons.message),
-                  Icon(Icons.contact_page),
-                ]),
-          ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("금호동3가"),
+        actions: const [
+          Icon(Icons.search),
+          Icon(Icons.menu),
+          Icon(CupertinoIcons.bell_fill)
+        ],
+      ),
+      body: Container(
+        height: 150,
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Image.asset(
+              'assets/dog.jpg',
+              width: 150,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('캐논 DSLR 100D (단렌즈.충전기 16기가SD 포함)'),
+                  Text('성동구 행당동 끌올 10분 전'),
+                  Text('210,000원'),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [Icon(Icons.favorite), Text('4')])
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
